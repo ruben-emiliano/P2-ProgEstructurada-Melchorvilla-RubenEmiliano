4019d1167656a7e15b7c9ba6462a2a0a1c390a21
emilianO <rubenemilianomelchorvilla@gmail.com>
2026-05-25 08:25:44 -0600

    funcion que abarca la libreria de datetime y de random

--- monitor_entrenamiento.py
+++ monitor_entrenamiento.py
@@ -17,10 +17,61 @@ import sys
 MAX_EPOCHS = 10
 UMBRAL_ERROR_CRITICO =0.95
 
+#obtenemos la informacion del dispositivo del usuario mediante la libreria sys
 def obtener_info_sistema():
     print("--informacion del sistema del usuario--")
     print("plataforma" , sys.platform)
     print("version de python", sys.version)
     print("argumentos recibidos", sys.argv)
     print("salida", sys.exit(1))
-    
+
+def simular_metricas_entrenamiento(cantidad_epochs):
+    inicio = datetime.datetime.now()
+    print("inicio de simulacion:",
+      inicio.strftime("%d/%m/%Y %H:%M:%S"))
+    lista_loss = []
+    lista_latencia = []
+
+    eventos = [
+        "Epoch exitoso",
+        "Gradiente inestable",
+        "Actualizacion de pesos"
+    ]
+
+    contador = 1
+
+    while contador <= cantidad_epochs:
+
+        # random.uniform()
+        loss = random.uniform(0.1, 1.0)
+
+        # random.random()
+        probabilidad = random.random()
+
+        # random.choice()
+        evento = random.choice(eventos)
+
+        latencia = random.uniform(0.5, 2.0)
+
+        lista_loss.append(loss)
+        lista_latencia.append(latencia)
+
+        print("epoch:", contador)
+        print("loss:", round(loss, 3))
+        print("probabilidad:", round(probabilidad, 3))
+        print("evento:", evento)
+        print("latencia:", round(latencia, 2))
+        print("--------------------")
+
+        contador += 1
+
+    fin = datetime.datetime.now()
+
+    print("fin de simulacion:",
+          fin.strftime("%d/%m/%Y %H:%M:%S"))
+
+    tiempo_total = fin - datetime.datetime.now()
+
+    print("tiempo transcurrido:", tiempo_total)
+
+    return lista_loss, lista_latencia
