f32d05d26b16869e67dc6a501f8e538151d9f6e0
emilianO <rubenemilianomelchorvilla@gmail.com>
2026-05-25 07:46:29 -0600

    importacion de las 5 librerias

---
+++ monitor_entrenamiento.py
@@ -0,0 +1,15 @@
+"""
+Nombre del Alumno: Rubén Emiliano Melchor Villa
+Matricula: UX25II324
+Fecha: 25/05/26
+Examen segundo parcial-Programacion Estructurada
+"""
+
+#importacion de las 5 librerias a uso del examen 
+
+import datetime
+import math
+import random
+import statistics
+import sys
+
